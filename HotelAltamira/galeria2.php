8f714f711fbf56120ce187b74ee60eeb7a6eccf7
Dylan Altamirano <doaltamiranoce@est.utn.ac.cr>
2017-04-13 20:04:46 -0600

    cambios en las extensiones de los archivos

--- HotelAltamira/galeria2.php
+++ HotelAltamira/galeria2.php
@@ -1,77 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Hotel Altamira</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/modern-business.css" rel="stylesheet">
-      <link rel="stylesheet" href="css/signinstyles.css">
-	   <link rel="stylesheet" href="css/lightbox.css">
-
-    <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-</head>
-
-<body>
-
-
-      <!-- Navigation of the website-->
-      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-          <div class="container">
-              <!-- Brand and toggle get grouped for better mobile display -->
-              <div class="navbar-header">
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                      <span class="sr-only">Toggle navigation</span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                  </button>
-                  <a class="navbar-brand" href="index.html">Hotel Altamira</a>
-              </div>
-              <!-- Collect the nav links, forms, and other content for toggling -->
-              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                  <ul class="nav navbar-nav navbar-right">
-                      <li>
-                          <a href="about.html">Acerca</a>
-                      </li>
-                      <li>
-                          <a href="services.html">Servicios</a>
-                      </li>
-                      <li>
-                          <a href="contact.html">Contáctenos</a>
-                      </li>
-                      <li class="dropdown">
-                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Galería <b class="caret"></b></a>
-                          <ul class="dropdown-menu">
-
-                              <li>
-                                  <a href="portfolio-4-col.html">Imagénes del hotel</a>
-                              </li>
-
-                          </ul>
-                      </li>
-                    </li>
-
-                    <li> <button type="button" class="btn btn-warning navbar-btn" name="sign-in-button">Entrar</button></li>
-
-                  </ul>
-              </div>
-              <!-- /.navbar-collapse -->
-          </div>
-          <!-- /.container -->
-      </nav>
+<?php require_once('classes/header.php'); ?>
 
     <!-- Page Content -->
     <div class="container">
@@ -150,7 +77,7 @@
                     </li>
                     <li class="active">
                         <a href="#">2</a>
-                    </li>                
+                    </li>
                     <li>
                         <a href="#">&raquo;</a>
                     </li>
--- HotelAltamira/portfolio-4-col.php
+++ HotelAltamira/portfolio-4-col.php
@@ -1,149 +1,142 @@
 <?php require_once('classes/header.php'); ?>
-    <!-- Page Content -->
-    <div class="container">
+<!-- Page Content -->
+<div class="container">
 
-        <!-- Page Heading/Breadcrumbs -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Galería de Imágenes
-                    <small>Conozca nuestras humildes instalaciones</small>
-                </h1>
-                <ol class="breadcrumb">
-                    <li><a href="index.html">Inicio</a>
-                    </li>
-                    <li class="active">Galería</li>
-                </ol>
-            </div>
+    <!-- Page Heading/Breadcrumbs -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Galería de Imágenes
+                <small>Conozca nuestras humildes instalaciones</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="index.html">Inicio</a>
+                </li>
+                <li class="active">Galería</li>
+            </ol>
         </div>
-        <!-- /.row -->
+    </div>
+    <!-- /.row -->
 
-        <!-- Projects Row -->
-        <div class="row">
-            <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
-                </a>
-            </div>
+    <!-- Projects Row -->
+    <div class="row">
+        <div class="col-md-3 img-portfolio">
+            <a href="img/Galeria/Pequeñas/bar.jpeg" data-lightbox="gallery" data-title="Bar">
+                <img class="img-responsive img-hover" src="img/Galeria/Pequeñas/bar.jpeg" alt="Bar">
+            </a>
         </div>
-        <!-- /.row -->
-
-        <!-- Projects Row -->
-        <div class="row">
-            <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
-                </a>
-            </div>
+        <div class="col-md-3 img-portfolio">
+            <a href="img/Galeria/Pequeñas/bar2.jpeg" data-lightbox="gallery" data-title="Exterior">
+                <img class="img-responsive img-hover" src="img/Galeria/Pequeñas/bar2.jpeg" alt="Exterior">
+            </a>
         </div>
-        <!-- /.row -->
-
-        <!-- Projects Row -->
-        <div class="row">
-            <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-3 img-portfolio">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/750x450" alt="">
-                </a>
-            </div>
+        <div class="col-md-3 img-portfolio">
+            <a href="img/Galeria/Pequeñas/Lancha.jpeg" data-lightbox="gallery" data-title="Barco">
+                <img class="img-responsive img-hover" src="img/Galeria/Pequeñas/Lancha.jpeg" alt="Barco">
+            </a>
         </div>
-        <!-- /.row -->
-
-        <hr>
-
-        <!-- Pagination -->
-        <div class="row text-center">
-            <div class="col-lg-12">
-                <ul class="pagination">
-                    <li>
-                        <a href="#">&laquo;</a>
-                    </li>
-                    <li class="active">
-                        <a href="#">1</a>
-                    </li>
-                    <li>
-                        <a href="#">2</a>
-                    </li>
-                    <li>
-                        <a href="#">3</a>
-                    </li>
-                    <li>
-                        <a href="#">4</a>
-                    </li>
-                    <li>
-                        <a href="#">5</a>
-                    </li>
-                    <li>
-                        <a href="#">&raquo;</a>
-                    </li>
-                </ul>
-            </div>
+        <div class="col-md-3 img-portfolio">
+            <a href="img/Galeria/Pequeñas/Cabañas2.jpeg" data-lightbox="gallery" data-title="Tours">
+                <img class="img-responsive img-hover" src="img/Galeria/Pequeñas/Cabañas2.jpeg" alt="Tours">
+            </a>
         </div>
-        <!-- /.row -->
+    </div>
+    <!-- /.row -->
 
-        <hr>
+    <!-- Projects Row -->
+    <div class="row">
+        <div class="col-md-3 img-portfolio">
+            <a href="img/Galeria/Pequeñas/Cama.jpeg" data-lightbox="gallery" data-title="Cama">
+                <img class="img-responsive img-hover" src="img/Galeria/Pequeñas/Cama.jpeg" alt="Cama">
+            </a>
+        </div>
+        <div class="col-md-3 img-portfolio">
+            <a href="img/Galeria/Pequeñas/Cena.jpeg" data-lightbox="gallery" data-title="Cena">
+                <img class="img-responsive img-hover" src="img/Galeria/Pequeñas/Cena.jpeg" alt="Cena">
+            </a>
+        </div>
+        <div class="col-md-3 img-portfolio">
+            <a href="img/Galeria/Pequeñas/cenas.jpeg" data-lightbox="gallery" data-title="Atardecer">
+                <img class="img-responsive img-hover" src="img/Galeria/Pequeñas/cenas.jpeg" alt="Atardecer">
+            </a>
+        </div>
+        <div class="col-md-3 img-portfolio">
+            <a href="img/Galeria/Pequeñas/exterior.jpeg" data-lightbox="gallery" data-title="Cabañas">
+                <img class="img-responsive img-hover" src="img/Galeria/Pequeñas/exterior.jpeg" alt="Cabañas">
+            </a>
+        </div>
+    </div>
+    <!-- /.row -->
 
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Grupo Altamira 2017</p>
-                </div>
-            </div>
-        </footer>
+    <!-- Projects Row -->
+    <div class="row">
+        <div class="col-md-3 img-portfolio">
+            <a href="img/Galeria/Pequeñas/habitacion.jpeg" data-lightbox="gallery" data-title="Habitacion">
+                <img class="img-responsive img-hover" src="img/Galeria/Pequeñas/habitacion.jpeg" alt="Habitacion">
+            </a>
+        </div>
+  <div class="col-md-3 img-portfolio">
+            <a href="img/Galeria/Pequeñas/muelle.jpg" data-lightbox="gallery" data-title="Muelle">
+                <img class="img-responsive img-hover" src="img/Galeria/Pequeñas/muelle.jpg" alt="Muelle">
+            </a>
+        </div>
+        <div class="col-md-3 img-portfolio">
+            <a href="img/Galeria/Pequeñas/CabañasExterior6.jpg" data-lightbox="gallery" data-title="Cabaña exterior">
+                <img class="img-responsive img-hover" src="img/Galeria/Pequeñas/CabañasExterior6.jpg" alt="Cabaña exterior">
+            </a>
+        </div>
+     <div class="col-md-3 img-portfolio">
+            <a href="img/Galeria/Pequeñas/interior.jpeg" data-lightbox="gallery" data-title="Habitacion interior">
+                <img class="img-responsive img-hover" src="img/Galeria/Pequeñas/interior.jpeg" alt="Habitacion interior">
+            </a>
+        </div>
 
     </div>
-    <!-- /.container -->
+    <!-- /.row -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <hr>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/signinfunctions.js"></script>
+    <!-- Pagination -->
+    <div class="row text-center">
+        <div class="col-lg-12">
+            <ul class="pagination">
+                <li>
+                    <a href="#">&laquo;</a>
+                </li>
+                <li class="active">
+                    <a href="#">1</a>
+                </li>
+                <li>
+                    <a href="galeria2.php">2</a>
+                </li>
+                <li>
+                    <a href="galeria2.php">&raquo;</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; Grupo Altamira 2017</p>
+            </div>
+        </div>
+    </footer>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/lightbox.min.js" ></script>
+<script src="js/signinfunctions.js"></script>
 
 </body>
 
