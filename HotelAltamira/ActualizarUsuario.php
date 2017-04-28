faf07bef0da18e4855e5b5565d675dcd6972ff08
Dylan Altamirano <doaltamiranoce@est.utn.ac.cr>
2017-04-27 23:11:47 -0600

    cambios en actualizar usuario cuenta

--- HotelAltamira/ActualizarUsuario.php
+++ HotelAltamira/ActualizarUsuario.php
@@ -11,48 +11,56 @@
   $nombre = $usuario->getNombre();
 
 ?>
+<!--Cuerpo de la pantalla de actualización-->
 <body>
-<div class="well">
-		<form id="form" action="classes/actualizacionUsuario.php" method="post" style="">
+		<div class="well">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3">
+					<h3>Actualizar mis datos</h3>
+				</div>
+				<div class="col-md-6 col-md-offset-3">
+					<form id="form" action="classes/actualizacionUsuario.php" method="post" style="">
 
-	<table width=500 >
-	<br>
-	<td >
-		<input class="form-control" type="text" placeholder=<?php echo $email;  ?> readonly>
+										<div class="form-group">
+											<input class="form-control" type="text" placeholder=<?php echo $email;  ?> readonly>
+										</div>
 
-          			   <div class="form-group">
-          					<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Nombre de usuario" value=<?php echo $nombre;  ?>>
-          			   </div>
-          				<div class="form-group">
-                              <label for="cbopaises">Elije tu país: </label>
-          									    <select class="form-control" name="cbopaises" tabindex="1">
-                                 <option value="Costa Rica">Costa Rica</option>
-                                 <option value="US">Estados Unidos</option>
-                                 <option value="Mexico">Mexico</option>
-                                 <option value="España">España</option>
-                                 <option value="Otro">Otro</option>
-          									    </select>
-          				</div>
-          				<div class="form-group">
-          						<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Contraseña actual">
-          				</div>
-						<div class="form-group">
-          					<input type="password" name="passwordA" id="passwordA" tabindex="2" class="form-control" placeholder="Contraseña nueva">
-          				</div>
-          				<div class="form-group">
-          						<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirmar contraseña nueva">
-          				</div>
-          				<div class="form-group">
-          						<div class="row">
-          						<div class="col-sm-6 col-sm-offset-3">
-          							<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Actualizar datos">
-          				</div>
-          				</div>
-          					</div>
-	</td>
-	</table>
-         </form>
-		 </div>
+												 <div class="form-group">
+													<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Nombre de usuario" value=<?php echo $nombre;  ?>>
+												 </div>
+												<div class="form-group">
+																		<label for="cbopaises">Elije tu país: </label>
+																			<select class="form-control" name="cbopaises" tabindex="1">
+																			 <option value="Costa Rica">Costa Rica</option>
+																			 <option value="US">Estados Unidos</option>
+																			 <option value="Mexico">Mexico</option>
+																			 <option value="España">España</option>
+																			 <option value="Otro">Otro</option>
+																			</select>
+												</div>
+												<div class="form-group">
+														<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Contraseña actual">
+												</div>
+												<div class="form-group">
+													<input type="password" name="passwordA" id="passwordA" tabindex="2" class="form-control" placeholder="Contraseña nueva">
+												</div>
+												<div class="form-group">
+														<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirmar contraseña nueva">
+												</div>
+												<div class="form-group">
+														<div class="row">
+														<div class="col-sm-6 col-sm-offset-3">
+															<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Actualizar datos">
+												</div>
+												</div>
+												</div>
+
+							 </form>
+				</div>
+
+			</div>
+
+		</div>
 </body>
 
         <!-- Footer -->
@@ -74,7 +82,7 @@
     <script src="js/bootstrap.min.js"></script>
 	 <script src="js/signinfunctions.js"></script>
 
-		
+
 
 
 </body>
